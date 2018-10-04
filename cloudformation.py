b08592b59d4f7796d0630696c47b8dcb2ccc5a61
Rian Wouters <rian.wouters@lighting.com>
2018-10-04 16:36:05 +0200

    iso overridng comment, override the EXPORT_LIST

--- cloudformation.py
+++ cloudformation.py
@@ -1,5 +1,11 @@
-def initialize(cli):
-    cli.register('building-command-table.cloudformation', inject_commands)
+from awscli.customizations.cloudformation.artifact_exporter import CloudFormationStackResource
+from awscli.customizations.cloudformation.artifact_exporter import EXPORT_LIST
 
-def inject_commands(command_table, session, **kwargs):
-    print 'cloudformation.inject_commands'
+def awscli_initialize(cli):
+    EXPORT_LIST.insert(0, CloudFormationStackResourceExtension)
+
+
+class CloudFormationStackResourceExtension(CloudFormationStackResource):
+    def do_export(self, resource_id, resource_dict, parent_dir):
+        print 'CloudFormationStackResourceExtension.do_export'
+        super(CloudFormationStackResourceExtension, self).do_export(resource_id, resource_dict, parent_dir)
--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
 from setuptools import setup
+from setuptools.command.install import install
+from subprocess import call
+from os import system
+
+class CustomInstall(install):
+    def run(self):
+        print 'CustomInstall'
+        install.run(self)
+        system("aws configure set plugins.cloudformation cloudformation")
 
 setup(
     name='awscli-plugins',
@@ -9,4 +18,7 @@ setup(
     install_requires=[
         'awscli',
     ],
+    cmdclass={
+        'install': CustomInstall
+    },
 )
