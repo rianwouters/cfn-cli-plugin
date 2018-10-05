feae4919b3947b64dc1228692806a63812273f28
Rian Wouters <rian.wouters@lighting.com>
2018-10-05 13:52:35 +0200

    debug

--- cloudformation.py
+++ cloudformation.py
@@ -5,8 +5,10 @@ org_make_abs_path = artifact_exporter.make_abs_path
 
 def make_abs_path(directory, path):
     for search_path in [' ' , 'node_modules']:
-        abs_path = org_make_abs_path(os.path.join(directory, search_path), path)
         print 'Searching ' + search_path
+        abs_path = org_make_abs_path(os.path.join(directory, search_path), path)
+        print abs_path
+        print os.path.isfile(abs_path)
         if os.path.isdir(abs_path) or os.path.isfile(abs_path):
             return abs_path
     return abs_path
