2ad54e41fd0ce73c20527c54a56195267d82274b
Rian Wouters <rian.wouters@lighting.com>
2018-10-05 12:24:55 +0200

    override make_abs_path to search multiple paths

--- cloudformation.py
+++ cloudformation.py
@@ -1,11 +1,15 @@
-from awscli.customizations.cloudformation.artifact_exporter import CloudFormationStackResource
-from awscli.customizations.cloudformation.artifact_exporter import EXPORT_LIST
+import awscli.customizations.cloudformation.artifact_exporter
+import os
+
+org_make_abs_path = artifact_exporter.make_abs_path
+
+def make_abs_path(directory, path):
+    for search_path in [' ' , 'node_modules']:
+        abs_path = org_make_abs_path(os.path.join(directory, search_path), path)
+        print 'Searching ' + search_path
+        if os.path.isdir(abs_path) or os.path.isfile(abs_path):
+            return abs_path
+    return abs_path
 
 def awscli_initialize(cli):
-    EXPORT_LIST.insert(0, CloudFormationStackResourceExtension)
-
-
-class CloudFormationStackResourceExtension(CloudFormationStackResource):
-    def do_export(self, resource_id, resource_dict, parent_dir):
-        print 'CloudFormationStackResourceExtension.do_export'
-        super(CloudFormationStackResourceExtension, self).do_export(resource_id, resource_dict, parent_dir)
+    artifact_exporter.make_abs_path = make_abs_path
