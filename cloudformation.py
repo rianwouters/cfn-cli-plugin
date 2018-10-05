7b16f34e26d139ff576486dbba967022feb7134b
Rian Wouters <rian.wouters@lighting.com>
2018-10-05 15:17:32 +0200

    first seach path entry should be empty

--- cloudformation.py
+++ cloudformation.py
@@ -4,11 +4,9 @@ import os
 org_make_abs_path = artifact_exporter.make_abs_path
 
 def make_abs_path(directory, path):
-    for search_path in [' ' , 'node_modules']:
+    for search_path in ['' , 'node_modules']:
         print 'Searching ' + search_path
         abs_path = org_make_abs_path(os.path.join(directory, search_path), path)
-        print abs_path
-        print os.path.isfile(abs_path)
         if os.path.isdir(abs_path) or os.path.isfile(abs_path):
             return abs_path
     return abs_path
