09f1df29d0416af812de008e0befddcd7f4099cc
Rian Wouters <rian.wouters@lighting.com>
2018-11-15 18:40:58 +0100

    only interpolate, no need to export

--- cloudformation.py
+++ cloudformation.py
@@ -1,6 +1,6 @@
 from awscli.customizations.cloudformation import artifact_exporter
 from awscli.customizations.cloudformation.artifact_exporter import Template, is_s3_url, is_local_file
-from awscli.customizations.cloudformation.yamlhelper import yaml_dump, yaml_parse
+from awscli.customizations.cloudformation.yamlhelper import yaml_parse
 from awscli.customizations.cloudformation.exceptions import InvalidTemplateUrlParameterError
 from os.path import join, exists, isabs
 from os import environ
@@ -29,7 +29,10 @@ def interpolate_local_includes(self, template_dict):
     if isinstance(template_dict, dict):
         for key, val in template_dict.items():
             if key == "Fn::Transform" and val.get("Name", None) == "AWS::Include":
-                self.includes.append(IncludeTransform(self, template_dict, key, val).imprt())
+                include = IncludeTransform(self.template_dir, val)
+                if include.local():
+                    del template_dict[key]
+                    template_dict.update(include.load())
             self.export_global_artifacts(val)
     elif isinstance(template_dict, list):
         for val in template_dict:
@@ -40,50 +43,25 @@ class IncludeTransform(object):
 
     PROPERTY_NAME = "Location"
 
-    def __init__(self, template, parent, key, val):
-        self.template = template
-        self.parent = parent
-        self.key = key
-        self.val = val
-        self.parameters = self.val.get("Parameters", {});
+    def __init__(self, dir, val):
+        self.dir = dir
+        self.parameters = val.get("Parameters", {});
         self.location = self.parameters.get(self.PROPERTY_NAME, {})
 
     def local(self):
         return not is_s3_url(self.location) and not self.location.startswith("https://s3.amazonaws.com/")
 
-    def imprt(self):
-        if self.local():
-            abs_path = make_abs_path(self.template.template_dir, self.location)
-            if not is_local_file(abs_path):
-                raise InvalidTemplateUrlParameterError(
-                    property_name=self.PROPERTY_NAME,
-                    resource_id="AWS::Include",
-                    template_path=abs_path)
+    def load(self):
+        abs_path = make_abs_path(self.dir, self.location)
+        if not is_local_file(abs_path):
+            raise InvalidTemplateUrlParameterError(
+                property_name=self.PROPERTY_NAME,
+                resource_id="AWS::Include",
+                template_path=abs_path)
 
-            del self.parent[self.key]
-            with open(abs_path, "r") as included_file:
-                self.parent.update(yaml_parse(included_file.read()))
-
-        return self
-
-    def export(self):
-        if self.local():
-            with mktempfile() as temporary_file:
-                temporary_file.write(yaml_dump(self.parent[self.key]))
-                temporary_file.flush()
-                self.parameters[self.PROPERTY_NAME] = self.template.uploader.upload_with_dedup(temporary_file.name, "template")
-                self.parent[self.key] = self.val
-        return self
-
-org_template_export = Template.export
-
-def export_template_with_includes(self):
-    d = org_template_export(self)
-    for include in self.includes:
-        include.export()
-    return d
+        with open(abs_path, "r") as included_file:
+            return yaml_parse(included_file.read())
 
 def awscli_initialize(cli):
     artifact_exporter.make_abs_path = make_abs_path
     Template.export_global_artifacts = interpolate_local_includes
-    Template.export = export_template_with_includes
